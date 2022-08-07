7f8d762a313cce25be509caf82c507a0593ee6b7
DFRZ7 <david.florez.ramirez@gmail.com>
2022-08-07 15:28:28 -0600

    Finalize Course
    Course finalization.

---
+++ learning-vue/src/components/MyComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="my-component">
+        
+            <h1>This is my component</h1>
+            <h2>{{ subtitle }}</h2>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MyComponent",
+    data() {
+        return {
+            subtitle:  "I'm the subtitle of my component"
+        }
+    }
+}
+</script>
